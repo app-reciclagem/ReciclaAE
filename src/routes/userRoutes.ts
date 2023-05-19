d9e9910b712dd9b7dfc5fe46659944e8d903bae0
Débora Camilly <debcamilly00@gmail.com>
2023-05-19 20:28:05 -0300

    feat: cria userController

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { CreateUserController } from "../controller/CreateUserController";
 import { SessionController } from "../controller/SessionController";
-
 const routes = Router();
 
 routes.post("/login", new SessionController().handle);
+routes.post("/users", new CreateUserController().handle);
 
 export { routes };
+
